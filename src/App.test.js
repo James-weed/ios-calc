359147316171a0a692282f4c1a8031a94c28a6ec
James-weed <146154085+James-weed@users.noreply.github.com>
2023-10-13 11:46:50 -0400

    Add App test feature

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,48 @@
-import { render, screen } from '@testing-library/react';
+// App.test.js
+import React from 'react';
+import { render } from '@testing-library/react';
 import App from './App';
+import { CalcProvider, useCalcContext } from './context';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('./context', () => ({
+  ...jest.requireActual('./context'),
+  useCalcContext: jest.fn(),
+}));
+
+jest.mock('./components/Calculator', () => {
+  return function MockCalculator() {
+    return (
+      <div data-testid="mock-calculator">
+        Mock Calculator
+      </div>
+    );
+  };
+});
+
+describe('App', () => {
+  it('renders App component with Calculator and Calculator Provider', () => {
+    const mockUseCalcContext = jest.fn();
+    mockUseCalcContext.mockReturnValue({
+      next: '2',
+      total: '5',
+      putNumber: jest.fn(),
+      putOperator: jest.fn(),
+      putSign: jest.fn(),
+      putPercentage: jest.fn(),
+      clear: jest.fn(),
+      calc: jest.fn(),
+    });
+
+    useCalcContext.mockImplementation(mockUseCalcContext);
+
+    const { getByTestId } = render(
+      <CalcProvider>
+        <App />
+      </CalcProvider>
+    );
+
+    // Add your assertions here
+    const calculatorElement = getByTestId('mock-calculator');
+    expect(calculatorElement).toBeInTheDocument();
+  });
 });
